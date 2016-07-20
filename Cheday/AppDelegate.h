197794eba46aa9adef71200a9c347c0a0a846af6
Andrey Krukovskiy <andrey.krukovskiy@gmail.com>
2016-07-20 14:30:21 +0300

    Добавлен Fabric.io и Cartfile

---
+++ Cheday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cheday
+//
+//  Created by Andrey Krukovskiy on 20.07.16.
+//  Copyright © 2016 Cheday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
