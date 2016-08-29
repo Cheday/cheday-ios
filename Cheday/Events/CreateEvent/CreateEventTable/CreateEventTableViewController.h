eb46e617f0c92e523d6a4ff62ed8c704681a3579
Andrey Krukovskiy <andrey.krukovskiy@gmail.com>
2016-08-29 12:12:51 +0300

    Рефакторинг

---
+++ Cheday/Events/CreateEvent/CreateEventTable/CreateEventTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateEventTableViewController.h
+//  Cheday
+//
+//  Created by Andrey Krukovskiy on 29.08.16.
+//  Copyright © 2016 Cheday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateEventTableViewController : UITableViewController
+
+@end
---
+++ Cheday/Events/CreateEvent/CreateEventTable/CreateEventTableViewController.m
@@ -0,0 +1,25 @@
+//
+//  CreateEventTableViewController.m
+//  Cheday
+//
+//  Created by Andrey Krukovskiy on 29.08.16.
+//  Copyright © 2016 Cheday. All rights reserved.
+//
+
+#import "CreateEventTableViewController.h"
+
+@interface CreateEventTableViewController ()
+
+@end
+
+@implementation CreateEventTableViewController
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+@end
